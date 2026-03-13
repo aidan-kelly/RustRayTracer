c89c234b726107e03a5904754ceb18e772574a78
Aidan Kelly <it.is.aidan@gmail.com>
2026-03-13 15:22:49 -0600

    Colour the sphere based on it's normal vectors.

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,31 @@ use vec3::{Point3, Vec3};
 use crate::vec3::{dot, unit_vector};
 
 // See if a ray hits a sphere
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64
 {
     let origin_to_center: Vec3 = *center - r.origin();
     let a = dot(r.direction(), r.direction());
     let b = -2.0 * dot(r.direction(), origin_to_center);
     let c = dot(origin_to_center, origin_to_center) - radius*radius;
     let discriminant = b*b - 4.0 * a * c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0
+    {
+        -1.0
+    } 
+    else 
+    {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: &Ray) -> Color
 {
-    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r)
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0
     {
-        return Color::new(1.0, 0.0, 0.0)
+        let N = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5*Color::new(N.x()+1.0, N.y()+1.0, N.z()+1.0);
     }
 
     let unit_direction: Vec3 = unit_vector(r.direction());
