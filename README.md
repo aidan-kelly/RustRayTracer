2cd4e2d3ec1f7faf7785a8a5f996d5b5fde4ec95
Aidan Kelly <it.is.aidan@gmail.com>
2026-03-16 19:54:04 -0600

    Finished! Generated the final render.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rust Ray Tracer
 
-Using [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) I am creating a ray tracer in Rust to help me learn the language. 
+Using [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) I created a ray tracer in Rust to help me learn the language. 
+
+![Rendered image of hundreds of spheres. Some diffuse, some metallic, some glass.](/final_result.png)
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod sphere;
 mod vec3;
 
 use crate::hittable_list::HittableList;
+use crate::rtweekend::{random_double, random_double_range};
 use crate::sphere::Sphere;
 use crate::camera::Camera;
 use crate::vec3::{Point3, Vec3};
@@ -24,34 +25,63 @@ fn main()
     // World 
     let mut world = HittableList::new();
 
-    let R = (PI/4.0);
+    let ground_material = Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
 
-    let material_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Arc::new(Dielectric::new(1.50));
-    let material_bubble = Arc::new(Dielectric::new(1.00/1.50));
-    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    for a in -11..11
+    {
+        for b in -11..11
+        {
+            let choose_mat = random_double();
+            let center = Point3::new(a as f64 + 0.9*random_double(), 0.2, b as f64 + 0.9 * random_double());
 
-    world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
-    world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.2), 0.5, material_center)));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
-    world.add(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.4, material_bubble)));
-    world.add(Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)));
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9
+            {
+                if choose_mat < 0.8
+                {
+                    let albedo = Color::random() * Color::random();
+                    let sphere_material = Arc::new(Lambertian::new(albedo));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+                else if choose_mat < 0.95
+                {
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = random_double_range(0.0, 0.5);
+                    let sphere_material = Arc::new(Metal::new(albedo, fuzz));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+                else
+                {
+                    let sphere_material = Arc::new(Dielectric::new(1.5));
+                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Arc::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material1)));
+
+    let material2 = Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material2)));
+
+    let material3 = Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material3)));
 
     let mut camera = Camera::default();
 
     camera.aspect_ratio = 16.0/9.0;
-    camera.image_width = 400;
-    camera.samples_per_pixel = 100;
+    camera.image_width = 1200;
+    camera.samples_per_pixel = 500;
     camera.max_depth = 50;
 
     camera.vfov = 20.0;
-    camera.lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    camera.lookat = Point3::new(0.0, 0.0, -1.0);
+    camera.lookfrom = Point3::new(13.0, 2.0, 3.0);
+    camera.lookat = Point3::new(0.0, 0.0, 0.0);
     camera.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    camera.defocus_angle = 10.0;
-    camera.focus_distance = 3.4;
+    camera.defocus_angle = 0.6;
+    camera.focus_distance = 10.0;
 
     camera.render(&world);
 }
